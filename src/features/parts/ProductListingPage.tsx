720acfd7d213f0f73f18517d921675d923914ad8
banschineer <tbanschb@gmail.com>
2025-09-08 13:03:50 -0400

    Fix ESLint error - use double quotes in console.log debug statement

--- src/features/parts/ProductListingPage.tsx
+++ src/features/parts/ProductListingPage.tsx
@@ -496,7 +496,8 @@ export const ProductListingPage: React.FC<ProductListingPageProps> = ({ onNav })
             </div>
           </div>
 
-          console.log('PLP DEBUG: Render conditions - isLoading:', isLoading, 'error:', !!error, 'isRateLimited:', isRateLimited, 'totalResults:', totalResults);
+      console.log("PLP DEBUG: Render conditions - isLoading:", isLoading, "error:", !!error, "isRateLimited:", isRateLimited, "totalResults:", totalResults);
+
           {/* Results */}
           {isLoading ? (
             <div className="flex justify-center items-center h-64">
